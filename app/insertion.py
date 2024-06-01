accde70e77312b7c5776ebe4ca63c5fa809bd51e
eshaan <sudaneshaan@gmail.com>
2024-06-01 13:02:28 +0530

    feat!: normalized json data and inserted it into database

---
+++ app/insertion.py
@@ -0,0 +1,22 @@
+from sqlalchemy import text
+
+import streamlit as st
+
+
+def insert_data(df_items,df_categories):
+    conn = st.connection('postgresql', 'sql')
+
+
+    with conn.session as s:
+        s.execute(text('DROP TABLE categories;'))
+        s.execute(text('DROP TABLE items;'))
+        s.execute(text('CREATE TABLE IF NOT EXISTS items (uid TEXT PRIMARY KEY,title TEXT,availability BOOLEAN,description TEXT, img TEXT,price INT,gender TEXT);'))
+        s.execute(text('CREATE TABLE IF NOT EXISTS categories(uid TEXT,level int,name text,PRIMARY KEY(uid,level),FOREIGN KEY(uid) REFERENCES items(uid));'))
+        
+        for index,row in df_items.iterrows():
+            s.execute(text('INSERT INTO items VALUES (:uid,:title,:availability,:description,:img,:price,:gender);'),
+            params=dict(uid=row['uniqueId'],title=row['title'],availability=row['availability'],description=row['productDescription'],img=row['productImage'],price=row['price'],gender=row['gender']))
+        for index,row in df_categories.iterrows():
+            s.execute(text('INSERT INTO categories VALUES (:uid,:level,:category)'),
+            params=dict(uid=row['uniqueId'],level=row['level'],category=row['cat']))
+        s.commit()
---
+++ app/normalization.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def normalized_dataframes(df_json):
+    list=['categoryType','productUrl','catlevel2Name','sku','catlevel3Name','catlevel1Name','catlevel4Name','name','size']
+    for col in df_json.columns:
+        if col in list:
+            del df_json[col]
+    df_categories=pd.DataFrame(columns=['uniqueId','level','cat'])
+    for index,row in df_json.iterrows():
+        levels=0
+        for category in row['category']:
+           df_categories.loc[len(df_categories.index)] = [row['uniqueId'],levels,category]
+           levels=levels+1
+    del df_json['category']
+    return df_json,df_categories
