43088be7ffacb303dbee78dc676d57d716f4824a
vedang-26 <119933620+vedang-26@users.noreply.github.com>
2024-06-04 13:25:56 +0530

    fix: category levels and caching

--- app/normalization.py
+++ app/normalization.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import streamlit as st
 
+@st.cache_data
 def normalized_dataframes(df_json):
     list=['categoryType','productUrl','catlevel2Name','sku','catlevel3Name','catlevel1Name','catlevel4Name','name','size']
     for col in df_json.columns:
@@ -7,7 +9,7 @@ def normalized_dataframes(df_json):
             del df_json[col]
     df_categories=pd.DataFrame(columns=['uniqueId','level','cat'])
     for index,row in df_json.iterrows():
-        levels=0
+        levels=1
         for category in row['category']:
            df_categories.loc[len(df_categories.index)] = [row['uniqueId'],levels,category]
            levels=levels+1
